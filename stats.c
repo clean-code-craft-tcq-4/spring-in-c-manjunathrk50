80fe7b6e46b3a0fee6ffb9f630de912775ab8b4e
manjunathrk50 <105204871+manjunathrk50@users.noreply.github.com>
2022-05-17 18:29:12 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -3,12 +3,8 @@
 
 #include <stdlib.h>
 #include <math.h>
+#define 0 NAN
 
-
-//struct Stats compute_statistics(float, int, struct);
-
-
-//struct Stats compute_statistics(const float* numberset, int setlength)
 struct Stats {
     
     float average;
@@ -16,10 +12,12 @@ struct Stats {
     float max;
 };
 
+void compute_statistics(float numberset[], int setlength, struct Stats *ptr);
+
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
-int main()
+/*int main()
 {
 	struct Stats s;
 	s.average = 0;
@@ -29,36 +27,59 @@ int main()
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = (int)sizeof(numberset) / sizeof(numberset[0]);
 	struct Stats compute_statistics(numberset[], setlength, &s);
-	
-	
+}*/
+
+const char *show_classification(double x) {
+    switch(fpclassify(x)) {
+        case FP_INFINITE:  return "Inf";
+        case FP_NAN:       return "NaN";
+        default:           return "unknown";
+    }
 }
 
 struct Stats compute_statistics(float numberset[], int setlength, struct Stats *ptr)
 {
 	int i,j;
 	float avg, sum=0;
-	for(i=0; i<setlength;i++)
-	{  for(j=0;j<setlength-i;j++)
-		{
-			if(numberset[i] > numberset[i+1])
-			{
-				int temp=numberset[i];
-				numberset[i]=numberset[i+1];
-				numberset[i+1]=temp;
-			}
-		}
-		printf("Displaying sorted elements of array\n");
-		for(i=0;i<setlength;i++)
-		{
-			printf("%f ", numberset[i]);
-			sum=sum+numberset[i];
-		}
-		printf("\n");
-		avg=(sum)/(setlength);
-		ptr->min=numberset[0];
-		ptr->max=numberset[setlength-1];
-		ptr->average = avg;
-	}
+	float mn=numberset[0], mx=numberset[0];
+	//char* ret=NULL;
 	
+	/*if(setlength == 0)
+	{
+		throw "Division by Zero condition ie NaN";
+	}*/
+	if(show_classification(0.0/0.0) == NaN)
+	{
+		ptr->min=0;
+		ptr->max=0;
+		ptr->average = 0;
+	}
+	else {
+	for(i=1;i<setlength;i++)
+		{  if(numberset[i]>mx)
+        {
+            mx = numberset[i];
+        }
+
+
+        if(numberset[i]<mn)
+        {
+            mn = numberset[i];
+        }
+			
+		}
+	 printf("Maximum element is : %f\n", mx);
+    printf("Minimum element is : %f\n", mn);
+	for(i=0;i<setlength;i++)
+	{
+		sum=sum+numberset[i];
+	}
+printf("sum=%f\n", sum);
+		avg=(sum)/(setlength);
+		ptr->min=mn;
+		ptr->max=mx;
+		ptr->average = avg;
+printf("ptr->min=%f ptr->max=%f ptr->average=%f\n", ptr->min, ptr->max, ptr->average);
+	}
 	
 }
