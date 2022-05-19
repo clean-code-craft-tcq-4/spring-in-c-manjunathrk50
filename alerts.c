6b8112c4f37cc7451a5e66cdcdcad3efafa5d99f
manjunathrk50 <105204871+manjunathrk50@users.noreply.github.com>
2022-05-19 18:53:58 +0530

    Create alerts.c

---
+++ alerts.c
@@ -0,0 +1,33 @@
+#include "alerts.h"
+#include "catch.hpp"
+
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+
+void emailAlerter(int maxThreshold, struct Stats *ptr)
+{
+    printf("we are in emailAlerter section\n");
+	if(ptr.avg > maxThreshold)
+	{ printf("email threshold is crossed\n");
+		emailAlertCallCount++;
+	}
+	else 
+		printf("email is out of range\n");
+}
+
+void ledAlerter(int maxThreshold, struct Stats *ptr)
+{
+    printf("we are in emailAlerter section\n");
+	if(ptr.avg > maxThreshold)
+	{ printf("led threshold crossed\n");
+		ledAlertCallCount++;
+	}
+	else 
+		printf("led is out of range\n");
+}
