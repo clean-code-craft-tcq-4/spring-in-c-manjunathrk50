f05c7b77325e5a8bf36c0d7401104092ea517487
manjunathrk50 <105204871+manjunathrk50@users.noreply.github.com>
2022-05-20 15:23:41 +0530

    Update alerts.h

--- alerts.h
+++ alerts.h
@@ -1,7 +1,7 @@
 void emailAlerter(int maxThreshold, struct Stats *ptr);
 void ledAlerter(int maxThreshold, struct Stats *ptr);
 
-void (*check_and_alert[])(int, *struct Stats)={emailAlerter, ledAlerter};
+void (*check_and_alert[])(int, struct *Stats)={emailAlerter, ledAlerter};
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
